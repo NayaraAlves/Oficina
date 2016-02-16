2b94ed280aee7f665c95fbf5c1080ac1542c53a4
Nayara Alves da Silva <nayara.silva@montreal.com.br>
2016-02-16 10:27:40 -0200

    Commit Inicial
    Commit Inicial

---
+++ Oficina/OficinaWin/PrincipalMDI.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace OficinaWin
+{
+  partial class PrincipalMDI
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+      this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+      this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.menuStrip1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // statusStrip1
+      // 
+      this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+      this.statusStrip1.Name = "statusStrip1";
+      this.statusStrip1.Size = new System.Drawing.Size(284, 22);
+      this.statusStrip1.TabIndex = 2;
+      this.statusStrip1.Text = "statusStrip1";
+      // 
+      // menuStrip1
+      // 
+      this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem});
+      this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+      this.menuStrip1.Name = "menuStrip1";
+      this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+      this.menuStrip1.TabIndex = 3;
+      this.menuStrip1.Text = "menuStrip1";
+      // 
+      // arquivoToolStripMenuItem
+      // 
+      this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+      this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+      this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+      this.arquivoToolStripMenuItem.Text = "Arquivo";
+      // 
+      // sairToolStripMenuItem
+      // 
+      this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+      this.sairToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.sairToolStripMenuItem.Text = "Sair";
+      this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+      // 
+      // PrincipalMDI
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.Controls.Add(this.statusStrip1);
+      this.Controls.Add(this.menuStrip1);
+      this.IsMdiContainer = true;
+      this.MainMenuStrip = this.menuStrip1;
+      this.Name = "PrincipalMDI";
+      this.Text = "PrincipalMDI";
+      this.menuStrip1.ResumeLayout(false);
+      this.menuStrip1.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.StatusStrip statusStrip1;
+    private System.Windows.Forms.MenuStrip menuStrip1;
+    private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+    private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+  }
+}
---
+++ Oficina/OficinaWin/PrincipalMDI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OficinaWin
+{
+  public partial class PrincipalMDI : Form
+  {
+    public PrincipalMDI()
+    {
+      InitializeComponent();
+    }
+
+    private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      this.Close();
+    }
+  }
+}
